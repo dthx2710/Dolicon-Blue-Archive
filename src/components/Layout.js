72391342f2d42d3543c5651006976507a9d823f3
Dolicon <dthx2710@hotmail.com>
2021-02-03 17:31:33 +0800

    Changed open graph image

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -45,7 +45,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix('/')}img/home-jumbotron.webp`}
+          content={`${withPrefix('/')}img/og-image.jpg`}
         />
       </Helmet>
       <Navbar />
