b3da565ded73f7e7dbf78d8944a6188ea4337735
Dolicon <dthx2710@hotmail.com>
2021-02-03 17:05:04 +0800

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -40,12 +40,12 @@ const TemplateWrapper = ({ children }) => {
         />
         <meta name="theme-color" content="#fff" />
 
-        <meta property="og:type" content="business.business" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix('/')}img/og-image.jpg`}
+          content={`${withPrefix('/')}img/home-jumbotron.webp`}
         />
       </Helmet>
       <Navbar />
