e59ae377531b40f5fcaf5b33dc39baf44a56103a
Dolicon <dthx2710@hotmail.com>
2021-02-07 15:41:35 +0800

    Filter dropdown template

--- src/components/Strikers.js
+++ src/components/Strikers.js
@@ -23,6 +23,7 @@ class Strikers extends React.Component {
     const { edges: strikers } = data.allMarkdownRemark
 
     return (
+      //Filter fields: Rarity/Weapon/Affiliation/Position/Role/ATK/DEF/Combat Advantage
       <div style={{backgroundColor:'#'}}>
         <div className="studentlist-header">
           <button className="button is-info is-light">Show Filters</button>
@@ -34,7 +35,93 @@ class Strikers extends React.Component {
           </p>
         </div>
         <div className="studentlist-filters">
-          Filter and search will be ready soon!
+          <div className="select is-info filter-item">
+            <select id="rarity">
+              <option value=''>Rarity</option>
+              <option value='3'>3*</option>
+              <option value='2'>2*</option>
+              <option value='1'>1*</option>
+            </select>
+          </div>
+
+          <div className="select is-info filter-item">
+            <select id="weapon">
+              <option value=''>Weapon</option>
+              <option value=''>HG</option>
+              <option value=''>SMG</option>
+              <option value=''>AR</option>
+              <option value=''>SR</option>
+              <option value=''>SG</option>
+              <option value=''>MG</option>
+              <option value=''>GL</option>
+              <option value=''>RG</option>
+              <option value=''>RF</option>
+              <option value=''>RL</option>
+              <option value=''>DualSMG</option>
+              <option value=''>DualMG</option>
+              <option value=''>MountMG</option>
+            </select>
+          </div>
+
+          <div className="select is-info filter-item">
+            <select id="affiliation">
+              <option value=''>Affiliation</option>
+              <option value='Abydos'>Abydos</option>
+              <option value='Trinity'>Trinity</option>
+              <option value='Gehenna'>Gehenna</option>
+              <option value='Millennium'>Millennium</option>
+              <option value='Red Winter'>Red Winter</option>
+              <option value='Valkyrie'>Valkyrie</option>
+              <option value='Hyakkiyako'>Hyakkiyako</option>
+              <option value='Shanhaijing'>Shanhaijing</option>
+            </select>
+          </div>
+
+          <div className="select is-info filter-item">
+            <select>
+              <option value=''>Position</option>
+              <option value=''>Front</option>
+              <option value=''>Middle</option>
+              <option value=''>Back</option>
+            </select>
+          </div>
+
+          <div className="select is-info filter-item">
+            <select>
+              <option value=''>Role</option>
+              <option value=''>Attacker</option>
+              <option value=''>Supporter</option>
+              <option value=''>Tank</option>
+              <option value=''>Healer</option>
+            </select>
+          </div>
+
+          <div className="select is-info filter-item">
+            <select>
+              <option value=''>ATK</option>
+              <option value=''>Penetration</option>
+              <option value=''>Explosion</option>
+              <option value=''>Mystic</option>
+            </select>
+          </div>
+
+          <div className="select is-info filter-item">
+            <select>
+              <option value=''>DEF</option>
+              <option value=''>Light Armour</option>
+              <option value=''>Heavy Armour</option>
+              <option value=''>Special Armour</option>
+            </select>
+          </div>
+
+          <div className="select is-info filter-item">
+            <select>
+              <option value=''>Combat Advantage</option>
+              <option value=''>Outdoor</option>
+              <option value=''>Urban</option>
+              <option value=''>Indoor</option>
+            </select>
+          </div>
         </div>
         <div className="columns is-multiline is-centered is-vcentered is-mobile">
           {strikers &&
