3edd4f461121d6b2dc337b62530a308222df4808
Dolicon <dthx2710@hotmail.com>
2021-12-22 16:36:38 +0800

    Update Map.js
    trying ads

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,6 +6,7 @@ class Map extends React.Component {
     super()
     this.state = {map:''};
     this.selectMap=this.selectMap.bind(this);
+    const ads = <script data-ad-client="ca-pub-8500505837042857" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> ;
   }
   selectMap(map){
     this.setState({map:map});
@@ -27,6 +28,7 @@ class Map extends React.Component {
         <p className="column is-12 is-size-6">
             Map images sourced from wikiru.jp - Area 3/4 hard images unavailable for now
         </p>
+        {ads}
       </div>
     )
   }
